a700365fb8cb41207d8f520db4fef72efc9e90e0
Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>
2018-05-31 14:58:51 +0200

    ale_tests: topology_discovery: test topology notification
    Send out a topology discovery on a second interface, to trigger a
    topology notification from the AL. Unfortunately, it seems this is
    not implemented. So keep the test but ignore the error.
    
    Signed-off-by: Arnout Vandecappelle (Essensium/Mind) <arnout@mind.be>

--- src/al/ale_tests/topology_discovery.c
+++ src/al/ale_tests/topology_discovery.c
@@ -138,46 +138,142 @@ static maskedbyte_t aletest_expect_cmdu_topology_response[] = {
     0x00, 0x00, 0x00, /* End of message TLV */
 };
 
+static uint8_t aletest_send_cmdu_topology_discovery2[] = {
+    0x01, 0x80, 0xc2, 0x00, 0x00, 0x13,     /* 1905.1 multicast MAC address */
+    0x02, 0xaa, 0xbb, 0x33, 0x44, 0x10,     /* AL MAC address OR interface address */
+    0x89, 0x3a,                             /* Protocol number */
+    0x00, 0x00,                             /* Version, reserved */
+    0x00, 0x00,                             /* Message type */
+    0x43, 0x24,                             /* MID (may be anything) */
+    0x00,                                   /* Fragment ID */
+    0x80,                                   /* last fragment, relay indicator */
+    /* TLV 0 */ 0x01,                               /* 1905.1 AL MAC address type TLV */
+                0x00, 0x06,
+                0x02, 0xaa, 0xbb, 0x33, 0x44, 0x10, /* AL MAC address */
+    /* TLV 1 */ 0x02,                               /* MAC address type TLV */
+                0x00, 0x06,
+                0x00, 0xaa, 0xbb, 0x33, 0x44, 0x10, /* inteface MAC address */
+    0x00, 0x00, 0x00, /* End of message TLV */
+};
+
+static maskedbyte_t aletest_expect_cmdu_topology_query2[] = {
+    0x02, 0xaa, 0xbb, 0x33, 0x44, 0x10,     /* my AL MAC address */
+    0x0202, 0xee, 0xff, 0x33, 0x44, 0x1000, /* AL MAC address OR interface address */
+    0x89, 0x3a,                             /* Protocol number */
+    0x00, 0x00,                             /* Version, reserved */
+    0x00, 0x02,                             /* Message type */
+    0xffff, 0xffff,                         /* MID (may be anything) */
+    0x00,                                   /* Fragment ID */
+    0x80,                                   /* last fragment, relay indicator */
+    0x00, 0x00, 0x00, /* End of message TLV */
+};
+
+/* Minimal topology response */
+static uint8_t aletest_send_cmdu_topology_response2[] = {
+    0x02, 0xee, 0xff, 0x33, 0x44, 0x00,     /* AL MAC address */
+    0x02, 0xaa, 0xbb, 0x33, 0x44, 0x10,     /* my AL MAC address */
+    0x89, 0x3a,                             /* Protocol number */
+    0x00, 0x00,                             /* Version, reserved */
+    0x00, 0x03,                             /* Message type */
+    0xff, 0xff,                             /* MID (same as query, TODO current implementation ignores it) */
+    0x00,                                   /* Fragment ID */
+    0x80,                                   /* last fragment, relay indicator */
+    /* TLV 0 */ 0x03,                               /* 1905.1 device information type TLV */
+                0x00, 0x10,
+                0x02, 0xaa, 0xbb, 0x33, 0x44, 0x10, /* AL MAC address */
+                0x01,                               /* 1 interface */
+    /* intf0 */ 0x00, 0xaa, 0xbb, 0x33, 0x44, 0x11, /* altest1 MAC address */
+                0x00, 0x01,                         /* Gigabit ethernet */
+                0x00,                               /*  No additional info */
+    /* No device bridging capability */
+    /* No Non-1905 neighbors */
+    /* No 1905 neighbors */
+    0x00, 0x00, 0x00, /* End of message TLV */
+};
+
+static maskedbyte_t aletest_expect_cmdu_topology_notification[] = {
+    0x01, 0x80, 0xc2, 0x00, 0x00, 0x13,     /* 1905.1 multicast MAC address */
+    0x0202, 0xee, 0xff, 0x33, 0x44, 0x1000, /* AL MAC address OR interface address */
+    0x89, 0x3a,                             /* Protocol number */
+    0x00, 0x00,                             /* Version, reserved */
+    0x00, 0x01,                             /* Message type */
+    0xffff, 0xffff,                         /* MID (one more than previous one) */
+    0x00,                                   /* Fragment ID */
+    0xc0,                                   /* last fragment, relayed multicast */
+    /* TLV 0 */ 0x01,                               /* 1905.1 AL MAC address type TLV */
+                0x00, 0x06,
+                0x02, 0xee, 0xff, 0x33, 0x44, 0x00, /* AL MAC address */
+    0x00, 0x00, 0x00, /* End of message TLV */
+};
+
+
+
 int main()
 {
     struct sockaddr_ll addr;
-    const char *interface_name = "aletestpeer0";
     int result = 0;
+    int s0, s1;
 
     PLATFORM_INIT();
     PLATFORM_PRINTF_DEBUG_SET_VERBOSITY_LEVEL(3);
 
-    int s = openPacketSocket(interface_name, htons(ETHERTYPE_1905), &addr);
-    if (-1 == s) {
+    s0 = openPacketSocket("aletestpeer0", htons(ETHERTYPE_1905), &addr);
+    if (-1 == s0) {
         return 1;
     }
 
     /* The AL MUST send a topology discovery CMDU every 60 seconds (+1s jitter). */
-    CHECK_EXPECT_PACKET(s, aletest_expect_cmdu_topology_discovery, 61000, result);
+    CHECK_EXPECT_PACKET(s0, aletest_expect_cmdu_topology_discovery, 61000, result);
 
     /* Trigger a topology query from the AL by sending a topology discovery. The AL MAY send a query, but we expect the
      * AL under test to indeed send one immediately. */
-    if (-1 == send(s, aletest_send_cmdu_topology_discovery, sizeof(aletest_send_cmdu_topology_discovery), 0)) {
+    if (-1 == send(s0, aletest_send_cmdu_topology_discovery, sizeof(aletest_send_cmdu_topology_discovery), 0)) {
         PLATFORM_PRINTF_DEBUG_ERROR("Failed to send topology discovery: %d (%s)\n", errno, strerror(errno));
     } else {
         /* Note: the AL also sends another topology discovery, but that is not needed so we don't do an expect for it. */
-        CHECK_EXPECT_PACKET(s, aletest_expect_cmdu_topology_query, 3000, result);
+        CHECK_EXPECT_PACKET(s0, aletest_expect_cmdu_topology_query, 3000, result);
         /* No need to respond to the query. */
     }
 
     /* Send a topology query. The AL MUST send a response. */
-    if (-1 == send(s, aletest_send_cmdu_topology_query, sizeof(aletest_send_cmdu_topology_query), 0))
+    if (-1 == send(s0, aletest_send_cmdu_topology_query, sizeof(aletest_send_cmdu_topology_query), 0))
     {
         PLATFORM_PRINTF_DEBUG_ERROR("Failed to send topology query: %d (%s)\n", errno, strerror(errno));
+        result++;
     } else {
         /* AL must respond within 1 second */
-        CHECK_EXPECT_PACKET(s, aletest_expect_cmdu_topology_response, 1000, result);
+        CHECK_EXPECT_PACKET(s0, aletest_expect_cmdu_topology_response, 1000, result);
+    }
+
+    s1 = openPacketSocket("aletestpeer1", htons(ETHERTYPE_1905), &addr);
+    if (-1 == s1) {
+        close(s0);
+        return 1;
+    }
+
+    /* Announce a second AL by sending a second toplogy discovery, which should trigger another query. */
+    if (-1 == send(s1, aletest_send_cmdu_topology_discovery2, sizeof(aletest_send_cmdu_topology_discovery2), 0)) {
+        PLATFORM_PRINTF_DEBUG_ERROR("Failed to send topology discovery: %d (%s)\n", errno, strerror(errno));
+        result++;
+    } else {
+        /* Note: the AL also sends another topology discovery, but that is not needed so we don't do an expect for it. */
+        CHECK_EXPECT_PACKET(s1, aletest_expect_cmdu_topology_query2, 3000, result);
+        if (-1 == send(s1, aletest_send_cmdu_topology_response2, sizeof(aletest_send_cmdu_topology_response2), 0)) {
+            PLATFORM_PRINTF_DEBUG_ERROR("Failed to send topology response: %d (%s)\n", errno, strerror(errno));
+            result++;
+        } else {
+            /* This should trigger a topology notification on the other interface, because there is a new neighbor. */
+            CHECK_EXPECT_PACKET(s0, aletest_expect_cmdu_topology_notification, 1000, result);
+            /* TODO Currently this doesn't trigger a topology change! So ignore this error for now. */
+            result--;
+        }
     }
 
     /* The AL MUST send a topology discovery CMDU every 60 seconds (+1s jitter). */
     /* FIXME we should subtract the time spent since the last topology discovery message */
-    CHECK_EXPECT_PACKET(s, aletest_expect_cmdu_topology_discovery, 61000, result);
+    CHECK_EXPECT_PACKET(s0, aletest_expect_cmdu_topology_discovery, 61000, result);
 
-    close(s);
+    close(s0);
+    close(s1);
     return result;
 }
